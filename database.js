9e71aeaf838d9ed363dc15ac6d5a6d190b9d6b2c
nonsodaboss-ops <nonsodaboss@gmail.com>
2026-03-01 13:25:44 +0100

    Update MongoDB connection string to use placeholder

--- database.js
+++ database.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb+srv://user1:user1secure@cluster0.fkll7fi.mongodb.net/thinkbooks?appName=Cluster0');
+    await mongoose.connect('mongodb+srv://user1:<db-password>@cluster0.fkll7fi.mongodb.net/thinkbooks?appName=Cluster0');
     console.log('MongoDB connected successfully');
   } catch (error) {
     console.error('MongoDB connection failed:', error.message);
